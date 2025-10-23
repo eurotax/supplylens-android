539fc396c961cbb770be98749e47e20a34b715bc
eurotax <piotr@ebiuro24.pl>
2025-10-23 10:52:02 +0200

    Update theme: Premium dark with black & gold colors

--- core/ui/src/main/java/com/eurotax/supplylens/core/ui/theme/Color.kt
+++ core/ui/src/main/java/com/eurotax/supplylens/core/ui/theme/Color.kt
@@ -2,15 +2,42 @@ package com.eurotax.supplylens.core.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// SupplyLens Premium Dark Theme - Black & Gold
+val CarbonBlack = Color(0xFF0B0D10)
+val RoyalGold = Color(0xFFD4AF37)
+val DarkSurface = Color(0xFF1E232A)
+val PureWhite = Color(0xFFFFFFFF)
+val GoldAccent = Color(0xFFFFD700)
+val DarkGray = Color(0xFF808080)
+val LightGray = Color(0xFFB0B0B0)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
-
-// SupplyLens brand colors
-val SupplyLensPrimary = Color(0xFF1976D2)
-val SupplyLensSecondary = Color(0xFF03DAC6)
-val SupplyLensTertiary = Color(0xFFFF6F00)
+// Material 3 Dark Scheme
+val md_theme_dark_primary = RoyalGold
+val md_theme_dark_onPrimary = CarbonBlack
+val md_theme_dark_primaryContainer = Color(0xFF3A3520)
+val md_theme_dark_onPrimaryContainer = GoldAccent
+val md_theme_dark_secondary = Color(0xFFBFA97A)
+val md_theme_dark_onSecondary = CarbonBlack
+val md_theme_dark_secondaryContainer = DarkSurface
+val md_theme_dark_onSecondaryContainer = Color(0xFFDCCA9A)
+val md_theme_dark_tertiary = Color(0xFFA0CACD)
+val md_theme_dark_onTertiary = CarbonBlack
+val md_theme_dark_tertiaryContainer = Color(0xFF1F3335)
+val md_theme_dark_onTertiaryContainer = Color(0xFFBCE6E9)
+val md_theme_dark_error = Color(0xFFFFB4AB)
+val md_theme_dark_errorContainer = Color(0xFF93000A)
+val md_theme_dark_onError = CarbonBlack
+val md_theme_dark_onErrorContainer = Color(0xFFFFDAD6)
+val md_theme_dark_background = CarbonBlack
+val md_theme_dark_onBackground = PureWhite
+val md_theme_dark_surface = DarkSurface
+val md_theme_dark_onSurface = PureWhite
+val md_theme_dark_surfaceVariant = Color(0xFF2A2F37)
+val md_theme_dark_onSurfaceVariant = LightGray
+val md_theme_dark_outline = Color(0xFF8A8F97)
+val md_theme_dark_inverseOnSurface = CarbonBlack
+val md_theme_dark_inverseSurface = PureWhite
+val md_theme_dark_inversePrimary = Color(0xFF665D00)
+val md_theme_dark_surfaceTint = RoyalGold
+val md_theme_dark_outlineVariant = Color(0xFF45464F)
+val md_theme_dark_scrim = Color(0xFF000000)
